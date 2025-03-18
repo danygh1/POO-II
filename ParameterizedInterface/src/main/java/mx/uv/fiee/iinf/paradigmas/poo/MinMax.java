8def21f43508b825e1c2bd311197d5e07f62a421
danygh1 <danny16020110@gmail.com>
2025-03-18 16:45:13 -0600

    Add files via upload

---
+++ ParameterizedInterface/src/main/java/mx/uv/fiee/iinf/paradigmas/poo/MinMax.java
@@ -0,0 +1,31 @@
+package mx.uv.fiee.iinf.paradigmas.poo;
+
+public class MinMax<T extends Comparable<T>> implements IMinMax<T> {
+    private T[] values;
+
+    public MinMax(T[] values) {
+        this.values = values;
+    }
+
+    @Override
+    public T min() {
+        T min = values[0];
+        for (T value : values) {
+            if (value.compareTo(min) < 0) {
+                min = value;
+            }
+        }
+        return min;
+    }
+
+    @Override
+    public T max() {
+        T max = values[0];
+        for (T value : values) {
+            if (value.compareTo(max) > 0) {
+                max = value;
+            }
+        }
+        return max;
+    }
+}
---
+++ ParameterizedInterface/src/main/java/mx/uv/fiee/iinf/paradigmas/poo/Student.java
@@ -0,0 +1,25 @@
+package mx.uv.fiee.iinf.paradigmas.poo;
+
+public class Student implements Comparable<Student> {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        return Integer.compare(this.age, other.age);
+    }
+
+    @Override
+    public String toString() {
+        return "Student Nombre= " + name + " Edad= " + age ;
+    }
+}
